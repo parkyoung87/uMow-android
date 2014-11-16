f41fb61044155e864754b6beece435d958e7b2ad
j0natran <j0natran@gmail.com>
2014-11-16 14:48:48 -0600

    Added activity John

---
+++ app/src/main/java/com/umow/android/ActivityJohn.java
@@ -0,0 +1,18 @@
+package com.umow.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Jonathan on 11/16/2014.
+ */
+
+public class ActivityJohn extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_john);
+    }
+
+}
--- app/src/main/java/com/umow/android/ActivityLogin.java
+++ app/src/main/java/com/umow/android/ActivityLogin.java
@@ -1,7 +1,11 @@
 package com.umow.android;
 
 import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by xuejianyu on 11/16/14.
@@ -13,6 +17,15 @@ public class ActivityLogin extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-    }
+        Button buttonJohn = (Button) findViewById(R.id.activity_login_button_john);
 
+        View.OnClickListener onClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ActivityLogin.this, ActivityJohn.class);
+                startActivity(intent);
+            }
+        };
+        buttonJohn.setOnClickListener(onClickListener);
+    }
 }
