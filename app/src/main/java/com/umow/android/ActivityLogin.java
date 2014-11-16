1bb39d7e0ae59490d634490e9e7a66c7674bf3c0
Tom Yu <tom.yu@burstingbrains.com>
2014-11-16 13:48:33 -0600

    Added UtilToast and created stub files for ActivityLogin

---
+++ app/src/main/java/com/umow/android/ActivityLogin.java
@@ -0,0 +1,18 @@
+package com.umow.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by xuejianyu on 11/16/14.
+ */
+
+public class ActivityLogin extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+}
--- app/src/main/java/com/umow/android/ActivityUMow.java
+++ app/src/main/java/com/umow/android/ActivityUMow.java
@@ -1,29 +1,41 @@
 package com.umow.android;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.umow.android.util.UtilToast;
 
 
 public class ActivityUMow extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_umow);
+
+
+        Button buttonLogin = (Button) findViewById(R.id.activity_umow_button_loginActivity);
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View view) {
+                Intent intent = new Intent(ActivityUMow.this, ActivityLogin.class);
+                startActivity(intent);
+            }
+        });
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    @Override public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.activity_umow, menu);
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    @Override public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
